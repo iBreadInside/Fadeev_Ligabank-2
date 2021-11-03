c8168ac02a69720c7cde2e32171bad6aa85301a1
Владимир <nekro199107@gmail.com>
2021-11-03 22:39:07 +0300

    Стилизация шапки для мобильной версии

--- ligabank/src/components/header/header.jsx
+++ ligabank/src/components/header/header.jsx
@@ -5,12 +5,15 @@ import Logo from '../logo/logo';
 import Nav from '../nav/nav';
 import { BtnType } from '../../const';
 import ModalLogin from '../modal-login/modal-login';
+import LoginBtn from '../login-btn/login-btn';
 
 Modal.setAppElement('#root');
 
 export default function Header() {
   const [isModalOpened, setIsModalOpened] = useState(false);
+  const [isMobileNavHide, setIsMobileNavHide] = useState(true);
 
+  // Modal handlers
   const handleModalOpen = () => {
     setIsModalOpened(true);
   };
@@ -19,23 +22,38 @@ export default function Header() {
     setIsModalOpened(false);
   };
 
+  // Nav handlers
+  const handleOpenNav = () => {
+    setIsMobileNavHide(false);
+  };
+
+  const handleCloseNav = () => {
+    setIsMobileNavHide(true);
+  };
+
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
+    <header className={`${styles.header} ${isMobileNavHide ? '' : styles.header__navShown}`}>
+      <div className={styles.wrapper}>
+
         <nav className={styles.nav}>
-          <Logo/>
-          <Nav isFooter={false}/>
-          <button
-            type={BtnType.BTN}
-            className={styles.login}
-            onClick={handleModalOpen}>
-              <svg className={styles.loginImg} width='20' height='22' viewBox='0 0 20 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <path
-                 d='M2.22222 14.3H4.44444V19.8H17.7778V2.2H4.44444V7.7H2.22222V1.1C2.22222 0.808262 2.33929 0.528472 2.54766 0.322183C2.75603 0.115892 3.03865 0 3.33333 0H18.8889C19.1836 0 19.4662 0.115892 19.6746 0.322183C19.8829 0.528472 20 0.808262 20 1.1V20.9C20 21.1917 19.8829 21.4715 19.6746 21.6778C19.4662 21.8841 19.1836 22 18.8889 22H3.33333C3.03865 22 2.75603 21.8841 2.54766 21.6778C2.33929 21.4715 2.22222 21.1917 2.22222 20.9V14.3ZM8.88889 9.9V6.6L14.4444 11L8.88889 15.4V12.1H0V9.9H8.88889Z'
-                 fill='#1F1E25'/>
-              </svg>
-              Войти в Интернет-банк
-          </button>
+          <div className={styles.nav__control}>
+            <button
+              type={BtnType.BTN}
+              className={styles.nav__toggler}
+              onClick={isMobileNavHide ? handleOpenNav : handleCloseNav}
+            />
+            <Logo/>
+            <button
+              type={BtnType.BTN}
+              className={`${styles.morph} ${isMobileNavHide ? styles.morph__login : styles.morph__close}`}
+              onClick={isMobileNavHide ? handleModalOpen : handleCloseNav}
+            />
+          </div>
+
+          <div className={`${styles.menu} ${isMobileNavHide ? styles.menu__hide : ''}`}>
+            <Nav isFooter={false}/>
+            <LoginBtn withText onLoginCLick={handleModalOpen} />
+          </div>
         </nav>
       </div>
 
